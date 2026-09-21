906b67c1b0cabba1a7f18c7791ab15616204b399
PeterAbos <abos.petike@gmail.com>
2026-09-21 12:47:31 +0200

    counties show

--- resources/views/counties/show.blade.php
+++ resources/views/counties/show.blade.php
@@ -4,11 +4,13 @@
 
 <h1>{{ $county->name }}</h1>
 
+<table border="2">
+    <tr><td>Id: {{ $county->id }}</td></tr>
+    <tr><td>Name: {{ $county->name }}</td></tr>
+    <tr><td>badge_url: {{ $county->badge_url }}</td></tr>
+    <tr><td><img src="{{$county->badge_url}}" alt="Megye címer" width="10%"></td></tr>
+</table>
 
-<p>Id: {{ $county->id }}</p>
-<p>Name: {{ $county->name }}</p>
-<p>badge_url: {{ $county->badge_url }}</p>
-<img src="{{$county->badge_url}}" alt="Megye címer" width="10%">
 
     
 @endsection
